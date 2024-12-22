4a814ef8f63a4046f241ff49e19b143b19e07f9b
Florian Damhaut <flda@odoo.com>
2024-12-22 17:30:15 +0100

    Day5 + separator for helper

--- 2024/5.go
+++ 2024/5.go
@@ -1,5 +1,64 @@
 package main
 
-func five() {
+import (
+	"fmt"
+	"sort"
+)
 
+func five() {
+	data := toLineArray("5.in")
+	ruless := data[:1176]
+	rules := fiveMakeRules(ruless)
+	books := _toIntMatrix(data[1177:], ",")
+	a := fiveA(rules, books)
+	b := fiveB(rules, books)
+	fmt.Println(a, b)
+}
+
+func fiveMakeRules(rules []string) map[int]map[int]bool {
+	res := make(map[int]map[int]bool)
+
+	for _, rule := range rules {
+		ri := toIntArray(rule, "|")
+		if res[ri[0]] == nil {
+			res[ri[0]] = make(map[int]bool)
+		}
+		res[ri[0]][ri[1]] = true
+	}
+
+	return res
+}
+
+func fiveA(rules map[int]map[int]bool, books [][]int) int {
+	res := 0
+	for _, book := range books {
+		if fiveIsOrdered(rules, book) {
+			res += book[len(book)/2]
+		}
+	}
+	return res
+}
+
+func fiveB(rules map[int]map[int]bool, books [][]int) int {
+	res := 0
+	for _, book := range books {
+		if !fiveIsOrdered(rules, book) {
+			sort.Slice(book, func(i, j int) bool {
+				return rules[book[i]][book[j]]
+			})
+			res += book[len(book)/2]
+		}
+	}
+	return res
+}
+
+func fiveIsOrdered(rules map[int]map[int]bool, book []int) bool {
+	for i, v1 := range book[:len(book)-1] {
+		for _, v2 := range book[i+1:] {
+			if rules[v2][v1] {
+				return false
+			}
+		}
+	}
+	return true
 }
--- 2024/main.go
+++ 2024/main.go
@@ -25,10 +25,13 @@ func toLineArray(path string) []string {
 	return strings.Split(string(dat), "\n")
 }
 
-func toIntArray(s string) []int {
+func toIntArray(s string, sep ...string) []int {
 	var res []int
-
-	for _, v := range strings.Split(s, " ") {
+	separator := " "
+	if len(sep) > 0 {
+		separator = sep[0]
+	}
+	for _, v := range strings.Split(s, separator) {
 		i, err := strconv.Atoi(v)
 		if err == nil {
 			res = append(res, i)
@@ -38,11 +41,14 @@ func toIntArray(s string) []int {
 	return res
 }
 
-func toIntMatrix(path string) [][]int {
-	lineArray := toLineArray(path)
-	var res [][]int = make([][]int, len(lineArray))
-	for i, s := range lineArray {
-		res[i] = toIntArray(s)
+func toIntMatrix(path string, sep ...string) [][]int {
+	return _toIntMatrix(toLineArray(path), sep...)
+}
+
+func _toIntMatrix(strings []string, sep ...string) [][]int {
+	var res [][]int = make([][]int, len(strings))
+	for i, s := range strings {
+		res[i] = toIntArray(s, sep...)
 	}
 	return res
 }
