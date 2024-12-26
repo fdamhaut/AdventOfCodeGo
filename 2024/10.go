ddbf4b78736e87c28841d455f70d52f7f1ba96a9
Florian Damhaut <flda@odoo.com>
2024-12-26 17:39:09 +0100

    day10

--- 2024/10.go
+++ 2024/10.go
@@ -1,5 +1,116 @@
 package main
 
-func ten() {
+import "fmt"
 
+func ten() {
+	carte := toLineArray("10.in")
+	a := tenSolveA(carte)
+	b := tenSolveB(carte)
+	fmt.Println(a, b)
+}
+
+type tenPos struct {
+	x   int
+	y   int
+	val byte
+}
+
+var tenNEXTDICT = map[byte]byte{
+	'0': '1',
+	'1': '2',
+	'2': '3',
+	'3': '4',
+	'4': '5',
+	'5': '6',
+	'6': '7',
+	'7': '8',
+	'8': '9',
+}
+
+func (p *tenPos) next(carte []string) []tenPos {
+	res := make([]tenPos, 0, 4)
+	x, y := p.x, p.y
+	if p.x > 0 && carte[x-1][y] == tenNEXTDICT[p.val] {
+		res = append(res, tenPos{x: x - 1, y: y, val: tenNEXTDICT[p.val]})
+	}
+	if p.x < len(carte)-1 && carte[x+1][y] == tenNEXTDICT[p.val] {
+		res = append(res, tenPos{x: x + 1, y: y, val: tenNEXTDICT[p.val]})
+	}
+	if p.y > 0 && carte[x][y-1] == tenNEXTDICT[p.val] {
+		res = append(res, tenPos{x: x, y: y - 1, val: tenNEXTDICT[p.val]})
+	}
+	if p.y < len(carte[0])-1 && carte[x][y+1] == tenNEXTDICT[p.val] {
+		res = append(res, tenPos{x: x, y: y + 1, val: tenNEXTDICT[p.val]})
+	}
+	return res
+}
+
+func (p *tenPos) hash(carte []string) int {
+	return p.x*len(carte[0]) + p.y
+}
+
+func (p *tenPos) numPaths(carte []string) int {
+	poses := []tenPos{*p}
+	res := make(map[int]bool)
+	for len(poses) > 0 {
+		newP := make([]tenPos, 0, 4*len(poses))
+		for _, p := range poses {
+			for _, np := range p.next(carte) {
+				if np.val == '9' {
+					res[np.hash(carte)] = true
+				} else {
+					newP = append(newP, np)
+				}
+			}
+		}
+		poses = newP
+	}
+	return len(res)
+}
+
+func (p *tenPos) numPathsB(carte []string) int {
+	poses := []tenPos{*p}
+	res := 0
+	for len(poses) > 0 {
+		newP := make([]tenPos, 0, 4*len(poses))
+		for _, p := range poses {
+			for _, np := range p.next(carte) {
+				if np.val == '9' {
+					res += 1
+				} else {
+					newP = append(newP, np)
+				}
+			}
+		}
+		poses = newP
+	}
+	return res
+}
+
+func tenSolveA(carte []string) int {
+	res := 0
+	for _, pos := range tenStart(carte) {
+		res += pos.numPaths(carte)
+	}
+	return res
+}
+
+func tenSolveB(carte []string) int {
+	res := 0
+	for _, pos := range tenStart(carte) {
+		res += pos.numPathsB(carte)
+	}
+	return res
+}
+
+func tenStart(carte []string) []tenPos {
+	res := make([]tenPos, 0, 100)
+	for i, line := range carte {
+		for j, r := range line {
+			if r == '0' {
+				res = append(res, tenPos{x: i, y: j, val: '0'})
+			}
+		}
+	}
+	return res
 }
--- 2024/9.go
+++ 2024/9.go
@@ -1,5 +1,50 @@
 package main
 
-func nine() {
+import "fmt"
 
+func nine() {
+	data := toLineArray("9.in")
+	a := nineSolveA(data[0])
+	fmt.Println(a)
+}
+
+const R2I = 48
+
+func nineSolveA(data string) int {
+	idx := 0
+	reverse := len(data) - 1
+	reverse_left := int(data[reverse]) - 48
+
+	pos := 0
+	res := 0
+
+	for idx < reverse {
+		full := int(data[idx]) - 48
+		empty := int(data[idx+1]) - 48
+
+		res += (idx / 2) * (full) * (2*pos + full - 1) / 2
+		fmt.Println("F", pos, idx/2, full, (idx/2)*(full)*(2*pos+full-1)/2)
+
+		pos += full
+
+		for empty > 0 {
+			amount := min(reverse_left, empty)
+			res += (reverse / 2) * (amount) * (2*pos + amount - 1) / 2
+			fmt.Println("R", pos, reverse/2, amount, (reverse/2)*(amount)*(2*pos+amount-1)/2)
+			reverse_left -= amount
+			if reverse_left <= 0 {
+				reverse -= 2
+				reverse_left = int(data[reverse]) - 48
+			}
+			empty -= amount
+			pos += amount
+		}
+		idx += 2
+		if idx == reverse {
+			fmt.Println("F", pos, reverse/2, reverse_left, (reverse/2)*(reverse_left)*(2*pos+reverse_left-1)/2)
+			res += (reverse / 2) * (reverse_left) * (2*pos + reverse_left - 1) / 2
+		}
+	}
+
+	return res
 }
